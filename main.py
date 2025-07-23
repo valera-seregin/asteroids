43defafc72bbc0e2c048aecfce614ec90d18d4e8
valera-seregin <valera-s-38@mail.ru>
2025-07-23 17:01:20 +0300

    optimize CPU resources usage

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -10,6 +12,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
