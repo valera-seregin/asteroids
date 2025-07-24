190c17762cfe684e065f659ee1a9750616beb18e
valera-seregin <valera-s-38@mail.ru>
2025-07-24 17:48:23 +0300

    add left and right spaceships rotations

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from constants import *
 
 def main():
     pygame.init()
+
     clock = pygame.time.Clock()
+
     dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
+
         player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
