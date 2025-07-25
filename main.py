d81c3d2ef9f711100d464e9984169d1dc5f4f297
valera-seregin <valera-s-38@mail.ru>
2025-07-25 13:16:22 +0300

    add groups to manage multiple game objects

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from constants import *
 def main():
     pygame.init()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     clock = pygame.time.Clock()
 
     dt = 0
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -20,11 +21,14 @@ def main():
 
         screen.fill((0, 0, 0))
 
-        player.draw(screen)
-        player.update(dt)
+        for drw in drawable:
+            drw.draw(screen)
+
+        updatable.update(dt)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
+
